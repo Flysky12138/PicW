a5750ada0519e9306d6b9dd00c184c2ecd847f4c
Flysky12138 <x2797299614@gmail.com>
2022-11-23 15:46:31 +0800

    feat: record scrolling position

--- src/plugins/router/index.ts
+++ src/plugins/router/index.ts
@@ -5,7 +5,14 @@ import routes from './routes'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes
+  routes,
+  scrollBehavior: (to, from, savedPosition) => {
+    if (savedPosition) {
+      return savedPosition
+    } else {
+      return { top: 0 }
+    }
+  }
 })
 
 router.beforeEach(to => {
